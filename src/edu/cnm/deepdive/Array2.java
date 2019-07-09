82a5c0a720542b7cc18ab432e3479334ca234b88
austindewitt13 <50061410+austindewitt13@users.noreply.github.com>
2019-07-08 22:20:09 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/Array2.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive;
+
+public class Array2 {
+
+    public boolean either24(int[] nums) {
+        /*I need serious help with everything.
+        I feel like I'm stuck in week 2 or 3 still.*/
+    }
+
+    public int[] pre4(int[] nums) {
+
+    }
+}
---
+++ test/edu/cnm/deepdive/Array2Test.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Array2Test {
+
+    @Test
+    void either24() {
+    }
+
+    @Test
+    void pre4() {
+    }
+}
